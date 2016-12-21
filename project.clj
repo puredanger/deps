6d3bd0d12edaa5729ff755b8de8c74a827100e02
puredanger <alex.miller@cognitect.com>
2016-12-21 11:48:41 -0600

    add groupId

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject deps "0.0.1"
+(defproject puredanger/deps "0.0.1"
   :description "deps"
   :url "https://github.com/puredanger/deps"
   :license {:name "Eclipse Public License"
@@ -7,4 +7,6 @@
   :aot :all
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.cemerick/pomegranate "0.3.1"]
-                 [org.clojure/tools.cli "0.3.5"]])
+                 [org.clojure/tools.cli "0.3.5"]]
+  :repositories [["clojars" {:url "https://clojars.org/repo/"
+                             :sign-releases false}]])
