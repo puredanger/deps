5e077e076cd8691dd212b3585a31832e5892debf
puredanger <alex.miller@cognitect.com>
2016-12-21 11:47:31 -0600

    proj meta

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject deps "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject deps "0.0.1"
+  :description "deps"
+  :url "https://github.com/puredanger/deps"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main deps.main
